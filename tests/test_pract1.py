1012a8a9b0dc08207fbdd172b1b356cbe6f48909
Himanshu Hazari <hazarihimanshu008@gmail.com>
2026-01-04 13:48:47 +0530

    fixing linting issue for test case

--- tests/test_pract1.py
+++ tests/test_pract1.py
@@ -1,8 +1,13 @@
-from sre_constants import ASSERT_NOT
 from pract1 import print_prime
 
 def test_prime_number():
+    """
+    Check prime number 
+    """
     assert print_prime(2) == [1,2]
 
 def test_no_prime_number():
+    """
+    Check prime number 
+    """
     assert print_prime(5) == [1,2,3,5]
